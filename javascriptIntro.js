0971561e5c7593d7adec1354ae0428327a0e0be8
U-Nexus\Gene <Gene@Nexus.(none)>
2013-06-22 13:00:16 -0400

    Added more stuff on arrays and how to run the script.

--- javascriptIntro.js
+++ javascriptIntro.js
@@ -1,5 +1,5 @@
 ///////////////////
-//// Variables ////
+//// VARIABLES ////
 ///////////////////
 
 var x = 1;
@@ -15,12 +15,19 @@ x = 'you can also use single quotes for strings';
 // Arrays are easy to declare
 // And they can hold objects of any type
 var array = ["this", 'is', "an 'array'", 3, true];
+console.log(array); // Should print "['this', 'is', 'an \'array\'', 3, true]"
+// It gets weird though
+// You can specify keys like this:
+array["someKey"] = "someValue";
 
+// So you can access "someValue" by doing array["someKey"] OR array.someKey (without quotes)
+// You can also access the first 5 elements by doing array[0] ... array[4] 
+// OR by doing array["0"] ... array["4"] 
 
-
+console.log(array); // should print "['this', 'is', 'an \'array\'', 3, true, someKey: 'someValue']"
 
 ///////////////////
-//// Functions ////
+//// FUNCTIONS ////
 ///////////////////
 
 // Functions can be declared in two ways:
@@ -53,3 +60,16 @@ console.log(result); // Should print 10
 // And the result will be 2, since the function doesn't check for a third parameter.
 
 
+
+
+/////////////////
+//// OBJECTS ////
+/////////////////
+
+
+// Coming soon...
+
+
+
+// To "run" this script, open a command prompt and navigate to the directory where this file is
+// Then run the command "node javascriptIntro.js"
