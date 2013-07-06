ab2882d3c40c63105ed42ef42d6d5016b718d619
Gene <gene.levitzky@gmail.com>
2013-07-06 03:11:43 -0400

    Added server app and some basic home page html.

---
+++ app.js
@@ -0,0 +1,56 @@
+/**
+ *  Module dependencies. Java "import" equivalent.
+ */
+var express = require('express')
+  , http    = require('http')
+  , path    = require('path');
+
+/**
+ *  Configuration and Middleware.
+ *  Setting the initial states of the "classes"
+ *  we're using.
+ */
+var app = express();
+app.configure(function(){
+  // Sets port number
+  app.set('port', process.env.PORT || 3000);
+  // Tells it where to find our HTML files
+  app.set('views', __dirname + '/views');
+  // Tells it what templates to use
+  app.set('view engine', 'ejs');
+  // Tells it to use favicons
+  app.use(express.favicon());
+  // ...
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({'secret': 'project-solstice'}));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  // Tells it where to found our javascript and css files
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
+
+/*
+ *  Routes.
+ */
+ /** If the server receives a request for "/",
+ Send them index.ejs.
+ */
+app.get('/', function(req, res) {
+    res.render('index');                
+});
+/** Ditto for "/index" */
+app.get('/index', function(req, res) {
+  res.render('index');
+});
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
